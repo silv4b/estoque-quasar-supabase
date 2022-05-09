db577ee9cd1e31a4834275d9ea7f6a2087bd3a52
Bruno Silva <brunno.linkin@gmail.com>
2022-05-08 23:08:07 -0300

    comentario-user-auth
    comentário das funções do componente de autenticação de usuário

--- src/composables/UserAuthUser.js
+++ src/composables/UserAuthUser.js
@@ -1,39 +1,62 @@
 import { ref } from "vue";
 import useSupabase from "src/boot/supabase";
 
+/**
+ * O usuário é definido fora da função useAuthUser para que
+ * atue como um estado global e sempre se refira a um único usuário.
+ * */
 const user = ref(null);
 
 export default function useAuthUser() {
 
   const { supabase } = useSupabase();
 
+  /**
+  * Login with email and password
+  */
   const login = async ({ email, password }) => {
     const { user, error } = await supabase.auth.signIn({ email, password });
     if (error) throw error
     return user;
   };
 
+  /**
+  * Login with google, github, etc
+  */
   const loginWithSocialProvider = async ({ provider }) => {
     const { user, error } = await supabase.auth.signIn({ provider });
     if (error) throw error
     return user;
   };
 
+  /**
+  * Logout
+  */
   const logout = async () => {
     const { error } = await supabase.auth.signOut;
     if (error) throw erro
   };
 
+  /**
+  * Check if the user is logged in or not
+  */
   const isLogged = () => {
     return !!user.value;
   };
 
+  /**
+  * Register
+  */
   const register = async ({ email, password, ...meta }) => {
     const { user, error } = await supabase.auth.signUp(
       { email, password },
       {
+        // arbitrary meta data is passed as the second argument under a data key
+        // to the Supabase signUp method
         data: meta,
-        // define a necessidad de validação do email cadastrado
+        // the to redirect to after the user confirms their email
+        // window.location wouldn't be available if we were rendering server side
+        // but since we're all on the client it will work fine
         redirectTo: `${window.location.origin}/me?fromEmail=registrationConfirmation`,
       }
     )
@@ -41,12 +64,18 @@ export default function useAuthUser() {
     return user;
   };
 
+  /**
+  * Update user email, password, or meta data
+  */
   const update = async (data) => {
     const { user, error } = await supabase.auth.update(data);
     if (error) throw error
     return user;
   };
 
+  /**
+  * Update user email, password, or meta data
+  */
   const sendPasswordRestEmail = async (email) => {
     const { user, error } = await supabase.auth.api.resetPasswordForEmail(email);
     if (error) throw error
